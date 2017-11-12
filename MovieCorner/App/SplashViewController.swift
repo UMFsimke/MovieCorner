ae75662d119a2cc6e18083efaac23763989999f4
Aleksandar Simic <simic.aleksandar87@gmail.com>
2017-11-12 19:28:56 +0100

    Add Splash View Controller

---
+++ MovieCorner/App/SplashViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SplashViewController.swift
+//  MovieCorner
+//
+//  Created by Aleksandar Simic on 11/12/17.
+//  Copyright © 2017 Slick. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUi()
+    }
+}
+
+//MARK: UI
+
+extension SplashViewController {
+    
+    fileprivate func setupUi() {
+        
+    }
+}
