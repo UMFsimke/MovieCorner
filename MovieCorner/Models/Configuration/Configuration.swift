19df5e12473b2c42fe586d6c639158301acb2d40
Aleksandar Simic <simic.aleksandar87@gmail.com>
2017-11-12 14:06:57 +0100

    feat: add release configuration
    Add release configuration

---
+++ MovieCorner/Models/Configuration/Configuration.swift
@@ -0,0 +1,9 @@
+//
+//  Configuration.swift
+//  MovieCorner
+//
+//  Created by Aleksandar Simic on 11/12/17.
+//  Copyright © 2017 Slick. All rights reserved.
+//
+
+import Foundation
---
+++ MovieCorner/Models/Configuration/ReleaseConfiguration.swift
@@ -0,0 +1,10 @@
+//
+//  ReleaseConfiguration.swift
+//  MovieCorner
+//
+//  Created by Aleksandar Simic on 11/12/17.
+//  Copyright © 2017 Slick. All rights reserved.
+//
+
+struct ReleaseConfiguration: ApplicationConfiguration {
+}
