1444d6feac2a40353c4ae43c15f2123fe2948cbc
Aleksandar Simic <simic.aleksandar87@gmail.com>
2017-11-12 14:07:47 +0100

    feat: add configuration model
    Add configuration that will be accessable through the application.

--- MovieCorner/Models/Configuration/Configuration.swift
+++ MovieCorner/Models/Configuration/Configuration.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2017 Slick. All rights reserved.
 //
 
-import Foundation
+struct Configuration {
+    fileprivate let configuration: ApplicationConfiguration
+    
+    init(configuration: ApplicationConfiguration = Configuration.currentConfiguration) {
+        self.configuration = configuration
+    }
+}
+
+//MARK: Application configuration variables
+
+extension Configuration {
+    public var apiHost: String {
+        return configuration.apiHost
+    }
+    
+    public var apiKey: String {
+        return configuration.apiKey
+    }
+}
+
+//MARK: Runtime determination
+
+fileprivate extension Configuration {
+    static var current: ConfigurationFlavor {
+        guard
+            let infoDictionary = Bundle.main.infoDictionary,
+            let configurationName = infoDictionary["Configuration"] as? String,
+            let buildConfiguration = ConfigurationFlavor(rawValue: configurationName)
+            else { return .Release }
+        
+        return buildConfiguration
+    }
+    
+    static var currentConfiguration: ApplicationConfiguration {
+        switch current {
+        case .Release, .Beta:
+            return ReleaseConfiguration()
+        case .Test:
+            return TestConfiguration()
+        }
+    }
+}
